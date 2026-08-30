3039c1956e286080e98a96291dd69d7b4d909418
Lawrence Quarshie <lquarshie29@gmail.com>
2026-08-30 12:49:30 -0500

    Added a .c file that demonstrates different data types in C.

---
+++ 5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int storage = 256;
+    float osversion = 26.3;
+    char usbtype = 'C';
+
+    printf("%d\n%f\n%c\n", storage, osversion, usbtype);
+    return 0;
+}
