5b488597b6167e45d2cfb27b5c5c7ea38e4de6cc
Lawrence Quarshie <lquarshie29@gmail.com>
2026-08-30 12:30:06 -0500

    Added a .c file that demonstrates different data types in C.

---
+++ 4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int mynum = 15; // integer (whole number)
+    float myfloatnum = 5.99; // floating point number
+    char myletter = 'D';
+
+    printf("%d\n", mynum);
+    printf("%f\n", myfloatnum);
+    printf("%c\n", myletter);
+    return 0;
+}
