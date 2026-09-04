d4c50806f637d521539a70406869979a9a78ea92
Lawrence Quarshie <lquarshie29@gmail.com>
2026-09-03 22:57:55 -0500

    Added 9.c file that demonstrates combining 2 different data types in a printf output.

---
+++ 9.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int total = 1246; // total listings on informacean.com
+    char grade = 'A'; // grade of items on informacean.com website
+
+    printf("There are %d items and they are all %c grade\n", total, grade);
+    return 0;
+}
