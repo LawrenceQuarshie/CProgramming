3e7416a73936fa32d9735322c43bbcad628c4315
Lawrence Quarshie <lquarshie29@gmail.com>
2026-09-09 00:20:51 -0500

    Added a B.c code which demonstrates assigning a value of one variable to another in C.

---
+++ c.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int mynum = 15;
+    int myothernum = 23;
+
+    // assign the value of myothernum (23) to mynum
+    mynum = myothernum;
+
+    printf("%d\n", mynum);
+    return 0;
+}
