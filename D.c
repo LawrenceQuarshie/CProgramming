b4dc7b4bc91ee1c6eddd96595cf2692252b12eb2
Lawrence Quarshie <lquarshie29@gmail.com>
2026-09-13 18:05:02 -0500

    Added a D.c file that demonstrates assigning the value of a variable to another in C.

---
+++ D.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // creates a variable and assign a value to it
+    int randnum = 9;
+
+    // create another variable
+    int myothernum;
+
+    // assign randnum to myothernum
+    myothernum = randnum;
+
+    printf("%d\n", myothernum);
+    return 0;
+}
