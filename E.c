f3d5416f7e4ec02e480bc1ae4259546c90a6ff9e
Lawrence Quarshie <lquarshie29@gmail.com>
2026-09-13 18:26:59 -0500

    Added a E.c file that demonstrates adding two variables together in C.

---
+++ E.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // random number
+    int x1 = 9;
+
+    // another random number
+    int x2 = 14;
+
+    // variables x1 and x2
+    int sumx = x1 + x2;
+
+    printf("%d\n", sumx);
+    return 0;
+}
