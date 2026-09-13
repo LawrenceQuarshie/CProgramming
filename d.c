073ac5fcab064bd20bf59ce4d645e5bce22db788
Lawrence Quarshie <lquarshie29@gmail.com>
2026-09-13 17:39:59 -0500

    Added a d.c file that demonstrates assigning the value of a variable to another in C.

---
+++ d.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // create a mynum variable and assign the value 15 to it
+    int mynum = 15;
+
+    // declare myothernum variable without assigning it a value
+    int myothernum;
+
+    // assign value of mynum to myothernum
+    myothernum = mynum;
+
+    printf("%d\n", myothernum);
+    return 0;
+}
