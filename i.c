ebbeb24afb85632d8ef778a885380b931d499fe9
Lawrence Quarshie <lquarshie29@gmail.com>
2026-09-20 11:52:10 -0500

    Added a H.c file that demonstrates how better naming of variables are necessary in C but other programming languages.

---
+++ i.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // good variable name
+    int minutesperhour = 60;
+
+    // okay, but not so easy to understand what m is actually is
+    int m = 60;
+
+    printf("%d\n", minutesperhour);
+    printf("%d\n", m);
+    return 0;
+}
