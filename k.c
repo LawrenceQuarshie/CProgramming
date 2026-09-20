4c22cfe617a132f0c62838339814e3b62d455980
Lawrence Quarshie <lquarshie29@gmail.com>
2026-09-20 17:13:23 -0500

    Added a k.c file that demonstrates performing math operations with integers in C.

---
+++ k.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // create integer variables
+    int length = 4;
+    int width = 6;
+    int area;
+
+    // calculates the area of a rectangle
+    area = length * width;
+
+    // prints the variables
+    printf("Length is: %d\n", length);
+    printf("Width is: %d\n", width);
+    printf("Area of the rectangle is: %d\n", area);
+    return 0;
+}
