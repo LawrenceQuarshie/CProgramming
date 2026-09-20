ac5a44f13bc77faceeb77e4d8a0ab079e7c08645
Lawrence Quarshie <lquarshie29@gmail.com>
2026-09-20 17:37:31 -0500

    Added l.c file that introduces different data types in C.

---
+++ l.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // create variables
+    int mynum = 5;
+    float myfloatnum = 5.9;
+    char myletter = 'D';
+
+    printf("%d\n", mynum);
+    printf("%f\n", myfloatnum);
+    printf("%c\n", myletter);
+    return 0;
+}
