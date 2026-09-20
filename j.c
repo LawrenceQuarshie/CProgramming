1e664f5bd537cf25f9979c6602fd894d7d0b3258
Lawrence Quarshie <lquarshie29@gmail.com>
2026-09-20 16:48:47 -0500

    Added j.c file that demonstrates the use different data types in a C program.

---
+++ j.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // student data
+    int studentid = 15;
+    int studentage = 23;
+    float studentfee = 75.25;
+    char studentgrade = 'B';
+
+    printf("Student id: %d\n", studentid);
+    printf("Student age: %d\n", studentage);
+    printf("Student fee: %f\n", studentfee);
+    printf("Student grade: %c\n", studentgrade);
+    return 0;
+}
