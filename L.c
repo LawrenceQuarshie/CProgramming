00e0300d202117bbe2fcf189a14f8c2d6784925d
kojo <lquarshie29@gmail.com>
2026-09-21 23:58:50 -0500

    Added a L.c file that demonstrates 3 different data types in C.

---
+++ L.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // dayton electric motor specs
+    int motorvoltage = 115; // integer
+    float motorcurrent = 5.3; // float
+    char motorclass = 'B'; // character
+
+    printf("%d\n", motorvoltage);
+    printf("%f\n", motorcurrent);
+    printf("%c\n", motorclass);
+    return 0;
+}
