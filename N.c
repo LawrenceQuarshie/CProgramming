5a9640ed8a51d39c8f41602d7d29330e5b80105e
kojo <lquarshie29@gmail.com>
2026-09-23 00:28:22 -0500

    Added a N.c file that demonstrates combining text and a number in a printf statement in C.

---
+++ N.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int randnum = 15;
+    char randtxt = 'H';
+
+    printf("The random number is %d and the random character is %c\n", randnum, randtxt);
+    return 0;
+}
